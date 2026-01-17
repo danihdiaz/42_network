8386f23d26298b3ff973635c27110bee102b7896
Daniel Hontanilla <dhontani@student.42madrid.com>
2026-01-17 13:21:33 +0100

    b

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhontani <dhontani@student.42.fr>          #+#  +:+       +#+        */
+/*   By: dhontani <dhontani@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026-01-16 16:03:17 by dhontani          #+#    #+#             */
-/*   Updated: 2026-01-16 16:03:17 by dhontani         ###   ########.fr       */
+/*   Created: 2026/01/16 16:03:17 by dhontani          #+#    #+#             */
+/*   Updated: 2026/01/17 12:57:41 by dhontani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,25 @@
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
     size_t	i;
-
+    size_t  j;
+    size_t  len;
+    
+    i = 0;
+    j = 0;
+    
     if (dstsize == 0)
+    {    
+        return (ft_strlen(src));
+    }    
 	
-
+    while (dst[i])
+        i++;
+    while (src[j] <= dstsize - 1 && src[j] != '\0')
+    {    
+        dst[i] = src[j];
+        i++;
+        j++;
+    }       
+    return (i);
 }
 
