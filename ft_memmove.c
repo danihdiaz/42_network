2f0576bdf4be13832f3e8bd51a9ab795a9610a4a
Daniel Hontanilla Diaz <dhontani@c1r16s3.42madrid.com>
2026-01-20 11:12:27 +0100

    g

--- ft_memmove.c
+++ ft_memmove.c
@@ -16,9 +16,31 @@ void *ft_memmove(void *dest, const void *src, size_t n)
 {
     unsigned char *d = (unsigned char *)dest;
     unsigned char *s = (unsigned char *)src;
-    size_t = i;
+    size_t i;
 
     i = 0;
-    while (i < n)
-    
+    if (dest == src)
+        return (dest);
+    if (n == 0)
+        return (dest);
+    if (d < s)
+    {
+        while (i < n)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }
+    else if (s < d)
+    {
+        i = n - 1;    
+        while (i > 0)
+        {
+            d[i] = s[i];
+            i--;
+        }
+        if (i == 0)
+            d[i] = s[i];
+    }
+    return (dest);
 }
