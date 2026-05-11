65497976a4464c5a8877f07e309d3fa10ba20b2e
Daniel Hontanilla Diaz <dhontani@c2r4s3.42madrid.com>
2026-05-11 14:19:32 +0200

    s

--- Python/milestone_02/modules_00_04/Python_00/ex6/ft_count_harvest_recursive.py
+++ Python/milestone_02/modules_00_04/Python_00/ex6/ft_count_harvest_recursive.py
@@ -1,9 +1,9 @@
-def ft_count_harvest_recursive(day = 1, limit = 0):
-	if (limit == 0):
-		days = int(input("Days until harvest: "))
-		limit = days
-	if (day <= limit):
-		print(f"Day {day}")
-		ft_count_harvest_recursive(day + 1, limit)
-	if (day == limit):
-		print("Harvest time!")
+def ft_count_harvest_recursive(day=1, limit=0):
+    if (limit == 0):
+        days = int(input("Days until harvest: "))
+        limit = days
+    if (day <= limit):
+        print(f"Day {day}")
+        ft_count_harvest_recursive(day + 1, limit)
+    if (day == limit):
+        print("Harvest time!")
