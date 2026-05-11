a2ee6ecfdf057ed8334bb4735d4a7337a5e4a8c1
Daniel Hontanilla Diaz <dhontani@c2r4s3.42madrid.com>
2026-05-11 14:14:06 +0200

    w

--- Python/milestone_02/modules_00_04/Python_00/ex5/ft_water_reminder.py
+++ Python/milestone_02/modules_00_04/Python_00/ex5/ft_water_reminder.py
@@ -1,6 +1,6 @@
 def ft_water_reminder():
-	dayspassed = int(input("Days since last watering: "))
-	if (dayspassed > 2):
+	days_passed = int(input("Days since last watering: "))
+	if (days_passed > 2):
 		print("Water the plants!")
 	else:
 		print("Plants are fine")
