7eb7e7a0842d5027ba63b5b4b6808105f753fa23
Daniel Hontanilla <dhontani@student.42madrid.com>
2026-05-18 20:45:21 +0200

    Python03 terminado

--- Python/milestone_02/modules_00_04/Python_03/ex5/ft_data_stream.py
+++ Python/milestone_02/modules_00_04/Python_03/ex5/ft_data_stream.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import Generator
+import random
 
 
 players = ["alice", "bob", "charlie", "dylan"]
@@ -8,4 +9,32 @@ actions = ["run", "eat", "sleep", "grab", "move", "climb", "swim"]
 
 def gen_event() -> Generator[tuple[str, str], None, None]:
 	while True:
-		yield 
+		yield (random.choice(players), random.choice(actions))
+
+
+def consume_event(prelist: list[tuple[str, str]]) -> Generator[tuple[str, str], None, None]:
+	while True:
+		item_to_cons = random.choice(prelist)
+		prelist.remove(item_to_cons)
+		yield item_to_cons
+	
+
+
+def main() -> None:
+	print("=== Game Data Stream Processor ===")
+	gen = gen_event()
+	for i in range(1000):
+		playr_action = next(gen)
+		print(f"Event {i}: Player {playr_action[0]} did action {playr_action[1]}")
+	list_ten = []
+	for _ in range (10):
+		list_ten.append(next(gen))
+	print(f"Built list of 10 events: {list_ten}")
+	for _ in range (10):
+		event = consume_event(list_ten)
+		print(f"Got event from list: {next(event)}")
+		print(f"Remains in list: {list_ten}")
+
+
+if __name__ == "__main__":
+	main()
---
+++ Python/milestone_02/modules_00_04/Python_03/ex6/ft_data_alchemist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import random
+
+names = ["Alice", "bob", "Charlie", "dylan", "Emma", "Gregory", "john", "kevin", "Liam"]
+
+def main() -> None:
+    print("=== Game Data Alchemist ===\n")
+    print(f"Initial list of players: {names}")
+    cap_names = [n.capitalize() for n in names]
+    print(f"New list with all names capitalized: {cap_names}")
+    only_cap_names = [n for n in names if n == n.capitalize()]
+    print(f"New list of capitalized names only: {only_cap_names}\n")
+    score_dict = {nombre: random.randint(0, 999) for nombre in cap_names}
+    print(f"Score dict: {score_dict}")
+    scr_avg = sum(score_dict.values()) / len(score_dict.values())
+    print(f"Score average is {round(scr_avg, 2)}")
+    dict_avb_avg = {key: value for key, value in score_dict.items() if value > scr_avg}
+    print(f"High scores: {dict_avb_avg}")
+
+
+if __name__ == "__main__":
+    main()
