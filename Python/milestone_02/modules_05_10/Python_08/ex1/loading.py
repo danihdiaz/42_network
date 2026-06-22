7b1bef4d9086560efe664168e787e161dbe10f1b
Daniel Hontanilla <dhontani@student.42madrid.com>
2026-06-22 18:08:40 +0200

    ex1 python8

--- Python/milestone_02/modules_05_10/Python_08/ex1/loading.py
+++ Python/milestone_02/modules_05_10/Python_08/ex1/loading.py
@@ -11,9 +11,11 @@ except ModuleNotFoundError:
     np = None
 
 try:
+    import matplotlib
     import matplotlib.pyplot as plt
 except ModuleNotFoundError:
     plt = None
+
 
 def dependency_checker() -> None:
     print("Checking dependencies:")
@@ -28,22 +30,35 @@ def dependency_checker() -> None:
     if plt is None:
         print("[KO] matplotlib not installed")
     else:
-        print(f"[OK] matplotlib ({plt.__version__}) - Visualization ready")
+        print(
+            f"[OK] matplotlib ({matplotlib.__version__}) - Visualization ready")
+try:
+    def data_gen() -> pd.DataFrame:
+        data = np.random.randn(1000)
+        df = pd.DataFrame(data, columns=["value"])
+        print("\nAnalyzing matrix data...")
+        print("Processing 1000 data points...")
+        return df
+except AttributeError:
+    pass
 
-
-def data_gen() -> pd.DataFrame:
-    print("\nAnalyzing matrix data...")
-    print("Processing 1000 data points...")
-    data = np.random.randn(1000)
-    df = pd.DataFrame(data, columns=["value"])
-    return df
-
-
-def png_gen(df: pd.DataFrame) -> None:
-    
-
+try:
+    def png_gen(df: pd.DataFrame) -> None:
+        print("Generating visualization...")
+        plt.hist(df["value"])
+        plt.savefig("matrix_analysis.png")
+except AttributeError:
+    pass
 
 if __name__ == "__main__":
     print("\nLOADING STATUS: Loading programs...\n")
     dependency_checker()
-    data_gen()
+    try:
+        df = data_gen()
+        png_gen(df)
+        print("\nAnalysis complete!")
+        print("Results saved to: matrix_analysis.png")
+    except Exception:
+        print("\nERROR: Run 'pip install -r requirements.txt' or 'poetry install' "
+              "to install the necessary dependencies"
+              )
