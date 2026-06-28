a1ed31cbf8556571ddce88b53809f8e94a897e30
Daniel Hontanilla <dhontani@student.42madrid.com>
2026-06-28 19:34:08 +0200

    listo

--- Python/milestone_02/modules_05_10/Python_08/ex2/oracle.py
+++ Python/milestone_02/modules_05_10/Python_08/ex2/oracle.py
@@ -1,1 +1,50 @@
 #!/usr/bin/env python3
+
+from dotenv import load_dotenv
+import os
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("\nORACLE STATUS: Reading the Matrix...\n")
+    config = {
+        "MATRIX_MODE": os.environ.get("MATRIX_MODE"),
+        "DATABASE_URL": os.environ.get("DATABASE_URL"),
+        "API_KEY": os.environ.get("API_KEY"),
+        "LOG_LEVEL": os.environ.get("LOG_LEVEL"),
+        "ZION_ENDPOINT": os.environ.get("ZION_ENDPOINT"),
+    }
+    flag = 0
+    for key, value in config.items():
+        if value is None or value == "":
+            print(f"[WARNING]: {key} not configured")
+            flag = 1
+    if flag == 0:
+        print(
+            "Configuration loaded\n"
+            f"Mode: {config['MATRIX_MODE']}\n"
+            "Database: Connected to local instance\n"
+            "API Access: Authenticated\n"
+            f"Log Level: {config['LOG_LEVEL']}\n"
+            "Zion Network: Online\n"
+        )
+    print("Environment security check:\n")
+    if flag == 0:
+        if config["MATRIX_MODE"] == "production":
+            print("WARNING: Running in production mode!\n")
+        else:
+            print("Running in development mode - debug enabled\n")
+        if config["MATRIX_MODE"] == "production" and config["LOG_LEVEL"] == "DEBUG":
+            print("[WARNING] Debug logging enabled in production!")
+        if os.environ.get("API_KEY") != config["API_KEY"]:
+            print("[KO] Harcoded secrets detected!!")
+        else:
+            print("[OK] No hardcoded secrets detected")
+        print(
+            "[OK] .env file properly configured\n"
+            "[OK] Production overrides available\n"
+        )
+
+    elif flag == 1:
+        print("[KO] .env file misconfigured\n")
+    print("The Oracle sees all configurations.")
+
