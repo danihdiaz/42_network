2178e71c19fe8a9de6716da87c37d7ee0b4efb12
Daniel Hontanilla <dhontani@student.42madrid.com>
2026-06-29 18:05:13 +0200

    r

---
+++ Python/milestone_02/modules_05_10/Python_09/ex0/space_station.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from pydantic import BaseModel, Field
+import datetime
+from typing import Optional
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    is_operational: bool = True
+    notes: Optional[str] = Field(max_length=200)
+
+
+def main() -> None:
+    print("Space Station Data Validation")
+    print("======================================")
+    try:
+        my_station = SpaceStation(station_id = "ISS001", name = "International Space Station", crew_size = 6, power_level = 85.5, oxygen_level = 92.3, notes=None)
+    except Exception as e:
+        print(f"{e}: Parameter misconfigured")
+        return
+    print("Valid station created:")
+    print(f"ID: {my_station.station_id}")
+    print(f"Name: {my_station.name}")
+    print(f"Crew: {my_station.crew_size}")
+    print(f"Power: {my_station.power_level}%")
+    print(f"Oxygen: {my_station.oxygen_level}%")
+    if my_station.is_operational is True:
+        print("Status: Operational\n")
+    else:
+        print("Status: Not operational\n")
+    print("======================================")
+    print("Expected validation error:")
+    try:
+        my_station = SpaceStation(station_id = "ISS00ewqrfdsfasdfasfsd1", name = "International Space Station", crew_size = 6, power_level = 85.5, oxygen_level = 92.3, notes=None)
+    except Exception as e:
+        print(f"Parameter misconfigured: {e}")
+        return
+
+if __name__ == "__main__":
+    main()
